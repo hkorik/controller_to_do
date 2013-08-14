91b04d6e12d1a05ac833d800d8dd73f00e88a781
Hershy Korik <hkorik@gmail.com>
2013-08-13 21:08:07 -0700

    controller_to_do_basic_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,28 @@ class UsersController < ApplicationController
 			redirect_to new_user_path
 		end
 	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success_message] = "User Successfully updated!"
+			redirect_to edit_user_path
+		else
+			flash[:error_messages] = @user.errors.full_messages
+			redirect_to edit_user_path
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id]).destroy
+		redirect_to users_path
+	end
 end
