fbe4830df82289b61e25ccd7227bf0dfaee4cf01
Hershy Korik <hkorik@gmail.com>
2013-08-13 18:25:29 -0700

    controller_to_do_basic_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
 	end
 
 	def new
-		
+
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success_message] = "User Successfully created!"
+			redirect_to new_user_path
+		else
+			flash[:error_messages] = @user.errors.full_messages
+			redirect_to new_user_path
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email_address, :first_name, :last_name, :password
-  
+  valid_email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  validates :email_address, :first_name, :last_name, :password, presence: true
+  validates :email_address, uniqueness: true, format: { with: valid_email_regex }
+
+
 end
